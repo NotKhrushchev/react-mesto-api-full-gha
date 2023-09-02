8994addb7b7ba8641b5e4d83159388c69a141d31
NickGeek1 <Nikitafilemine1@gmail.com>
2023-09-02 15:19:07 +0300

    feat: Добавлен крипто-ключ для токена

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,5 @@
 const { CREATED, OK } = require('http-status-codes').StatusCodes;
 const jwt = require('jsonwebtoken');
-const { NODE_ENV, JWT_SECRET } = process.env;
 
 const bcrypt = require('bcryptjs');
 
@@ -133,7 +132,7 @@ const login = (req, res, next) => {
           if (!matched) {
             throw new AuthorizationErr();
           }
-          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+          const token = jwt.sign({ _id: user._id }, 'key', { expiresIn: '7d' });
           return res.send({ token });
         });
     })
